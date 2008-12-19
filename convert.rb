e8689fd300e83e2190b69c16224b208d3ab51b4c
Harlan Crystal <harlan@173-101-160-100.pools.spcsdns.net>
2008-12-18 20:18:43 -0500

    the driver file

---
+++ convert.rb
@@ -0,0 +1,25 @@
+require 'ftools'
+require 'yaml'
+require 'erb'
+require 'rubygems'
+gem 'activerecord'
+require 'activerecord'
+require 'models_3_2/model32'
+require 'models_2_14_2/model2142'
+
+require 'models_2_14_2/bug2142'
+
+# parse db config file
+database_settings = YAML::load(ERB.new(IO.read("database.yml")).result)
+
+
+# connect to the two databases
+Model32.establish_connection(database_settings["bugzilla_3_2"])
+Bugzilla2142::Model2142.establish_connection(database_settings["bugzilla_2_14_2"])
+
+print "Truncating everything in the 3.2 database in preparation? (y/N) "
+line = gets.strip.upcase
+exit if line.empty? || line[0,1] != 'Y'
+
+
+puts "We have this many bugs: #{Bugzilla2142::Bug2142.count}"
